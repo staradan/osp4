611ec56d38643dfe9da417b02305044b013efd05
staradan <44818251+staradan@users.noreply.github.com>
2021-11-09 21:13:27 -0600

    f

--- Assign4_mon.c
+++ Assign4_mon.c
@@ -16,7 +16,30 @@ void writerFunction(){
 }
 
 int main(int argc, char const *argv[]){
-    //get the input
+    //check that it's ten
+    if(argc != 11){
+        puts("You must enter 10 inputs.  No more, no less silly!");
+        return 0;
+    }
+
+    for(int i = 1; i < argc; i = i+1 ){
+        //check for a and b
+        int binaryNum;
+
+        if(atoi(argv[i]) != 1 && atoi(argv[i]) != 0){
+            puts("lies!");
+        }
+    }
+
+
+
+    // for(int i = 1; i < argc; i = i+1 ){
+    //     if(atoi(argv[i]) == 1){
+    //         puts("one");
+    //     }else{
+    //         puts("zero");
+    //     }
+    // }
     //loop through the readers and writers
     return 0;
 }
