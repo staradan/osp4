ec569519aca09a013860635dfc6d9efa4e588365
cgjhusker <54635857+cgjhusker@users.noreply.github.com>
2021-11-11 00:10:26 -0600

    Updated Sem and Mon

--- Assign4_mon.c
+++ Assign4_mon.c
@@ -4,15 +4,69 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
+#include <pthread.h>
+#include <unistd.h> 
+#include <semaphore.h>
 
-//reader function
-void readerFunction(){
-    puts("reader");
-}
+int currIndex = 0;
+pthread_cond_t canRead;
+pthread_cond_t canWrite;
+pthread_mutex_t m;
+int NReaders = 0;
+int WaitingReaders = 0;
+int NWriters = 0;
+int WaitingWriters = 0;
+
 
 //writer function
-void writerFunction(){
-    puts("writer");
+void writerFunction(int i){
+    while (currIndex != i-1){
+
+    }
+
+    if (NWriters == 1 || NReaders > 0) {
+        ++WaitingWriters;
+        pthread_cond_wait(&canWrite, &m);
+        --WaitingWriters;
+    }
+
+    NWriters = 1;
+    currIndex++;
+
+    printf("Writer %d starts writing\n", i);
+    sleep(4); 
+    printf("Writer %d ends writing\n", i);
+    
+    NWriters = 0;
+    if (WaitingReaders){
+        pthread_cond_signal(&canRead);
+    } else {
+        pthread_cond_signal(&canWrite);
+    }
+}
+
+//reader function
+void readerFunction(int i){
+    while (currIndex != i-1){
+        
+    }
+
+    if (NWriters == 1 || WaitingWriters > 0) {
+        ++WaitingReaders;
+        pthread_cond_wait(&canRead, &m);
+        --WaitingReaders;
+    }
+  
+    ++NReaders;
+    
+    printf("Reader %d starts reading\n", i);
+    currIndex++;
+    sleep(4); 
+    printf("Reader %d ends reading\n", i);
+    
+    if (--NReaders == 0){
+        pthread_cond_signal(&canWrite);
+    }
 }
 
 int inputCheck(int argc, char const *argv[]){
@@ -22,7 +76,6 @@ int inputCheck(int argc, char const *argv[]){
     }
 
     for(int i = 1; i < argc; i = i+1 ){
-        //check for a and b
         if(strcmp("1",argv[i]) != 0 && strcmp("1",argv[i]) != 1){ // if it's not 0 or 1
             puts("You must enter either 0 or 1.  Nothing else silly!");
             return 0;
@@ -33,14 +86,27 @@ int inputCheck(int argc, char const *argv[]){
 }
 
 int main(int argc, char const *argv[]){
-    //check that it's ten
+    pthread_t read[10];
+    //pthread_mutex_init(&m, 0, 1); 
+    //pthread_cond_init(canRead, 0, 1);
+    //pthread_cond_init(canWrite, 0, 1);
 
+    //check that it's ten
     if(inputCheck(argc, argv) == 0){
         return 0;
     }
 
     for(int i = 1; i < argc; i = i+1 ){
         // make the threads
+        if (strcmp("0",argv[i]) == 0) {
+            pthread_create(&read[i], NULL, readerFunction, i);
+        } else {
+            pthread_create(&read[i], NULL, writerFunction, i);
+        } 
+    }
+
+    for(int i = 1; i < argc; i = i+1 ){
+        pthread_join(read[i], NULL);
     }
 
     return 0;
--- Assign4_sem.c
+++ Assign4_sem.c
@@ -4,15 +4,52 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
+#include <pthread.h>
+#include <unistd.h> 
+#include <semaphore.h>
 
-//reader function
-void readerFunction(){
-    puts("reader");
-}
+sem_t mutex;
+pthread_mutex_t lock;
+int numreader = 0;
+int currIndex = 0;
 
 //writer function
-void writerFunction(){
-    puts("writer");
+void writerFunction(int i){
+    while (currIndex != i-1){
+
+    }
+    sem_wait(&mutex);
+    currIndex++;
+    printf("Writer %d starts writing\n", i);
+    sleep(4); 
+    printf("Writer %d ends writing\n", i);
+    sem_post(&mutex);
+}
+
+//reader function
+void readerFunction(int i){
+    while (currIndex != i-1){
+        
+    }
+    pthread_mutex_lock(&lock);
+    numreader++;
+    if(numreader == 1) {
+        sem_wait(&mutex);
+    }
+    
+    pthread_mutex_unlock(&lock);
+    
+    printf("Reader %d starts reading\n", i);
+    currIndex++;
+    sleep(4); 
+    printf("Reader %d ends reading\n", i);
+
+    pthread_mutex_lock(&lock);
+    numreader--;
+    if(numreader == 0) {
+        sem_post(&mutex);
+    }
+    pthread_mutex_unlock(&lock);
 }
 
 int inputCheck(int argc, char const *argv[]){
@@ -22,7 +59,6 @@ int inputCheck(int argc, char const *argv[]){
     }
 
     for(int i = 1; i < argc; i = i+1 ){
-        //check for a and b
         if(strcmp("1",argv[i]) != 0 && strcmp("1",argv[i]) != 1){ // if it's not 0 or 1
             puts("You must enter either 0 or 1.  Nothing else silly!");
             return 0;
@@ -33,14 +69,26 @@ int inputCheck(int argc, char const *argv[]){
 }
 
 int main(int argc, char const *argv[]){
-    //check that it's ten
+    sem_init(&mutex, 0, 1); 
+    pthread_mutex_init(&lock, NULL);
+    pthread_t read[10];
 
+    //check that it's ten
     if(inputCheck(argc, argv) == 0){
         return 0;
     }
 
     for(int i = 1; i < argc; i = i+1 ){
         // make the threads
+        if (strcmp("0",argv[i]) == 0) {
+            pthread_create(&read[i], NULL, readerFunction, i);
+        } else {
+            pthread_create(&read[i], NULL, writerFunction, i);
+        } 
+    }
+
+    for(int i = 1; i < argc; i = i+1 ){
+        pthread_join(read[i], NULL);
     }
 
     return 0;
